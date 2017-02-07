af3c3af172b08c2771743ee9ae1bf2fc06e1863b
Hayden <hconlinmouat@gmail.com>
2017-02-07 03:23:00 -0800

    changed the icon again

--- desktop/src/com/reddy/boxdrop/desktop/DesktopLauncher.java
+++ desktop/src/com/reddy/boxdrop/desktop/DesktopLauncher.java
@@ -2,12 +2,11 @@ package com.reddy.boxdrop.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.reddy.geodrop.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Geo Drop";
+		config.title = "Box Drop";
 		config.width = com.reddy.boxdrop.Main.WIDTH;
 		config.height = com.reddy.boxdrop.Main.HEIGHT;
 		new LwjglApplication(new com.reddy.boxdrop.Main(), config);
